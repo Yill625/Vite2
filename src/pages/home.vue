46cfa8350fb9aa4e04de786f378450e2d6c26276
Ivan <1095036273@qq.com>
2021-03-03 20:48:37 +0800

    添加底部导航栏

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,1 +1,19 @@
-<template>11111</template>
+<template>
+  <van-tabbar v-model="active" safe-area-inset-bottom>
+    <van-tabbar-item icon="home-o">标签</van-tabbar-item>
+    <van-tabbar-item icon="search">标签</van-tabbar-item>
+    <van-tabbar-item icon="friends-o">标签</van-tabbar-item>
+    <van-tabbar-item icon="setting-o">标签</van-tabbar-item>
+  </van-tabbar>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  setup() {
+    const active = ref(0);
+    return { active };
+  },
+};
+</script>
